2c3209668023d284e41e9eedc2d07bb5ed0fb582
brianddk <brianddk@users.noreply.github.com>
2017-12-11 10:50:46 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # binance-fees
+
+A simple nodejs program to generate a report showing the smallest possible purchases.  Since Binance does not allow partial lots, there is often **dust** left in counter currencies after purchases or sales.  This will calculate the minimum lot cost on the various exchanges to give you an idea what can be bought with that dust.  It will also calculate the **exit tax** required to move the various coins off of binance.
+
+## Getting Started
+
+You'll need a desktop system with command console.  I'm using nodejs version **5.11.0** so if your using a later version, I recommend installing **nvm** to run other versions in parallel.
+
+### Prerequisites
+
+Ensure that you are running nodejs version 5.11.0
+
+```
+node -v
+```
+
+If you have `nvm` installed you can activate version 5.11.0
+
+```
+nvm use 5.11.0
+```
+
+### Installing
+
+Obtain the repository using git
+
+```
+git clone https://github.com/brianddk/binance-fees.git
+```
+
+Once you are on a good nodejs version, a simple `npm install` will suffice
+
+```
+npm install
+```
+
+This should be all that is required and you can no generate a report.
+
+### Running
+
+To run the report simply run `node binance-fees.js`
+
+```
+node binance-fees.js > report.csv
+```
+
+The output of the report will be a CSV file viewable by your favorite spreadsheet.
+
+## Built With
+
+* [Node.js](https://nodejs.org/en/) - The NodeJS language runtime
+* [npm](https://www.npmjs.com/) - NodeJS package manager
+* [nvm-windows](https://github.com/coreybutler/nvm-windows) - nvm for Windows, but there are many releases for other OSes too.
+
+## Contributing
+
+Just issue a PR on github.
+
+## Issues
+
+Just submit an issue on github.
+
+## Authors
+
+* **brianddk** - *Initial work* - [Github](https://github.com/brianddk) - Tips [LTC]: LQjSwZLigtgqHA3rE14yeRNbNNY2r3tXcA
+
+## License
+
+This project is licensed under the [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0) License.
+
+## Acknowledgments
+
+* Thanks to [Binance](https://www.binance.com/) for providing an API
---
+++ binance-fees.js
@@ -0,0 +1,147 @@
+// [rights]  Copyright Dan B. (brianddk) 2017 https://github.com/brianddk
+// [license] Licensed under Apache 2.0 https://www.apache.org/licenses/LICENSE-2.0
+// [repo]    https://github.com/brianddk/binance-fees
+// [tips]    LTC: LQjSwZLigtgqHA3rE14yeRNbNNY2r3tXcA
+//
+var rp = require('request-promise');
+var bluebird = require('bluebird');
+var cheerio = require('cheerio')
+
+// Globals (parameterize later).
+var bMarkdown = true; // else CSV
+var bTruncate = true; // only list < $1
+
+function jsonReq(url, bParse) {
+    return rp({
+        uri: url,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: bParse
+    });
+} 
+
+var binanceExch = jsonReq('https://api.binance.com/api/v1/exchangeInfo', true);
+var binanceQuot = jsonReq('https://api.binance.com/api/v3/ticker/price', true);
+var gdaxBtcQuot = jsonReq('https://api.gdax.com/products/BTC-USD/ticker', true);
+var gdaxEthQuot = jsonReq('https://api.gdax.com/products/ETH-USD/ticker', true);
+var binanceFees = jsonReq('https://support.binance.com/hc/en-us/articles/115000429332-Fee-Structure-on-Binance', false);
+//var gdaxExch     = rp('https://api.gdax.com/products/');
+
+bluebird.all([binanceExch, binanceQuot, gdaxBtcQuot, gdaxEthQuot, binanceFees])
+    .spread(function (exch, quote, btc, eth, feeHtml) {
+        var $ = cheerio.load(feeHtml);
+
+        var feelist = {};
+        var $rows = $("tbody tr");
+        $rows.each(function(i) {
+            if (!i) return;
+            var text = $(this).text();
+            var lines = text.split('\n');
+            var coin = lines[5].trim();
+            var fee = lines[4].trim();
+            if (isNaN(fee)) fee = 0;
+            feelist[coin] = {};
+            feelist[coin]["fee"] = fee;
+        });
+        
+        var quotelist = {}
+        for(var i in quote) {
+            var q = quote[i];
+            quotelist[q.symbol] = {};
+            quotelist[q.symbol]["price"] = q.price;            
+        }
+        
+        var usdPrice = {}
+        usdPrice["BTC"]  = btc.price;
+        usdPrice["ETH"]  = eth.price;
+        usdPrice["USDT"] = Math.min(btc.price / quotelist["BTCUSDT"].price,
+                                    eth.price / quotelist["ETHUSDT"].price);
+        usdPrice["BNB"]  = Math.min(btc.price * quotelist["BNBBTC"].price,
+                                    eth.price * quotelist["BNBETH"].price);
+                                    
+        var pricelist = {};
+        for(var i in exch.symbols) {
+            var symbol = exch.symbols[i].symbol;
+            var coin = exch.symbols[i].baseAsset;
+            var counter = exch.symbols[i].quoteAsset;
+            if(counter != "BTC" && counter != "ETH" && 
+               counter != "BNB" && counter != "USDT") {continue;}
+            for(var j in exch.symbols[i].filters) {
+                var ftype = exch.symbols[i].filters[j].filterType;
+                if (ftype == "LOT_SIZE") {
+                    break;
+                }
+            }
+            var minLot = exch.symbols[i].filters[j].minQty;
+            
+            var priceUsd = usdPrice[counter];            
+            var price = quotelist[symbol].price;
+            var fee = feelist[coin].fee;
+            pricelist[symbol] = {};
+            pricelist[symbol]["coin"] = coin;
+            pricelist[symbol]["counter"] = counter;
+            pricelist[symbol]["minLot"] = minLot;
+            pricelist[symbol]["price"] = price * minLot;
+            pricelist[symbol]["priceUsd"] = price * minLot * priceUsd;            
+            pricelist[symbol]["feeUsd"] = fee * price * priceUsd;
+        }
+        
+        var sortedKeys = [];
+        for(var symbol in pricelist) {
+            var n = {};
+            n["symbol"] = symbol;
+            n["maxUsd"] = Math.max(pricelist[symbol].priceUsd, pricelist[symbol].feeUsd)
+            if(bTruncate && n.maxUsd > 1.0) { continue; } 
+            sortedKeys.push(n);
+        }
+        
+        sortedKeys.sort(function(a, b) {
+            return a.maxUsd - b.maxUsd;
+        });
+        
+        var header=`
+Key
+
+- **symbol** - The currency pair being traded.
+- **coin** - The coin being bought.
+- **counter** - The currency used to pay for the purchase (counter currency).
+- **minLot** - The smallest amount of the coin that can be purchased.
+- **price** - The market price for the smallest amount of the coin.
+- **lotUsd** - The cost to buy the smallest amount in equivical USD.
+- **feeUsd** - The withdrawl fee for that coin (if paid in USD).
+
+|symbol|coin|counter|minLot|price|lotUsd|feeUsd|
+|---|---|---|---|---|---|---|`
+
+        if(bMarkdown) {
+            console.log(header);
+            for(var i in sortedKeys) {
+                var symbol = sortedKeys[i].symbol;
+                var p = pricelist[symbol];
+                var line = "|" + symbol + "|" + 
+                    p.coin + "|" +
+                    p.counter + "|" +
+                    Number(p.minLot).toFixed(6) + "|" +
+                    Number(p.price).toFixed(8) + "|" +
+                    Number(p.priceUsd).toFixed(4) + "|" +
+                    Number(p.feeUsd).toFixed(4) + "|";
+                console.log(line);
+            }
+        }
+        else {
+            console.log("symbol,coin,counter,minLot,price,lotUsd,feeUsd")
+            for(var i in sortedKeys) {
+                var symbol = sortedKeys[i].symbol;
+                var p = pricelist[symbol];
+                var line = symbol + "," + 
+                    p.coin + "," +
+                    p.counter + "," +
+                    Number(p.minLot) + "," +
+                    Number(p.price) + "," +
+                    Number(p.priceUsd) + "," +
+                    Number(p.feeUsd);
+                console.log(line);
+            }
+        }
+    });
